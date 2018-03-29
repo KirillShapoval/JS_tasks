f06fa320194510327b1e5155795da3a657ace23f
KirillShapoval <petechkin1vasiliy@gmail.com>
2018-03-29 20:39:34 +0300

    add data structures task_2

--- data_structures/1/main.js
+++ data_structures/1/main.js
@@ -1,4 +1,4 @@
-function removeEveryOdd(arr) {
+20:28 29.03.2018function removeEveryOdd(arr) {
   var oddItems = arr.filter(function(element, i) {
     return (i % 2 === 0);
   });
@@ -6,7 +6,9 @@ function removeEveryOdd(arr) {
   return oddItems;
 }
 
-var arr = [1, 5, '3', 4, 111, '112', 7, 8, "orange", 10, 11];
+var arr = [2, 5, '3', 4, 111, '112', 7, 8, "orange", 10, 11];
+
+removeEveryOdd(arr);
 
 var arrOddItems = removeEveryOdd(arr);
 console.log(arrOddItems);
---
+++ data_structures/2/main.js
@@ -0,0 +1,17 @@
+function arrPositiveSum(array) {
+  var sum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      sum += array[i];
+    }
+  }
+  return sum;
+}
+
+var arr = [1, -4, 7, 12, -5, 9, 5, -7];
+
+arrPositiveSum (arr);
+
+var checkSum = arrPositiveSum(arr);
+console.log(checkSum);
