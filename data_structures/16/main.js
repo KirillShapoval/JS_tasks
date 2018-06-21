d7147d803df2e86e12f9b8c170e83448333a4a5b
KirillShapoval <petechkin1vasiliy@gmail.com>
2018-06-21 15:14:11 +0300

    add 16

---
+++ data_structures/16/main.js
@@ -0,0 +1,34 @@
+﻿function transformStr(str) {
+  var arr = str.split("");
+  var obj = {};
+
+  arr.forEach(elem => {
+    var value = obj[elem];
+
+    if (!value) {
+      obj[elem] = 1;
+      return;
+    }
+
+    obj[elem] = value + 1;
+  });
+
+  var newStr = '';
+
+  for (var key in obj) {
+
+    if (obj[key] === 1) {
+     newStr += key;
+    }
+
+    else newStr += key + obj[key];
+
+  }
+
+  return newStr;
+}
+
+var str = "aaabbccccd";
+
+var testTransformStr = transformStr(str);
+console.log(testTransformStr);
