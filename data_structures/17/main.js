453b60384409349259d03cee4bb2ea3d1a0c67e2
KirillShapoval <petechkin1vasiliy@gmail.com>
2018-07-11 18:16:33 +0300

    add task 17

---
+++ data_structures/17/main.js
@@ -0,0 +1,22 @@
+﻿function searhAnagrams(anagrams) {
+  var obj = {};
+  var resultOfAnagrams = [];
+
+  for (var i = 0; i< anagrams.length; i++) {
+    var newAnagrams = anagrams[i].split("").sort().join("");
+    obj[newAnagrams] = anagrams[i];
+  }
+
+  for (var key in obj) {
+    resultOfAnagrams.push(obj[key]);
+  }
+
+    return resultOfAnagrams;
+
+}
+
+var anagrams = ["ратификация", "тарификация", "старорежимность",
+ "нерасторжимость", "лепесток", "телескоп"];
+
+var testSearhAnagrams = searhAnagrams(anagrams);
+console.log(testSearhAnagrams);
