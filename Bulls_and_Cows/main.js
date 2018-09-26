0951d6e26d132f45e59d55fdb7b410c1c043db01
KirillShapoval <petechkin1vasiliy@gmail.com>
2018-09-27 00:53:29 +0300

    some updates

--- Bulls_and_Cows/main.js
+++ Bulls_and_Cows/main.js
@@ -2,26 +2,28 @@
 
     // !!! Тhe word you need to guess is in console of browser !!!
     
+    const arrayOfIsograms = ['pie', 'bar', 'end', 'duty', 'dust', 'host', 'blast', 'store', 'layer', 'market', 'sailor', 'mosaic'];
+
+    const validResults = [];
+
     const formInput = document.getElementById('form-input');
-    const btnSubmit = document.getElementById('form');
-    const jsReload = document.getElementById('reload');
-    const endOfGame = document.getElementById('end-of-game');
     const wordVers = document.getElementById('wordvers');
     const playAgain = document.getElementById('playagain');
+
+    const isogramWord = getRandomArrayValue(arrayOfIsograms);
+    let tries = countTheNumbersOfTries(isogramWord);
+
+    console.log('Isogram (secret word) is ' + isogramWord);
     
-    btnSubmit.addEventListener('submit', handleSubmit);
-    jsReload.addEventListener('click', playingNewGame);
-    endOfGame.addEventListener('click', endOfTheGame); 
+    document.getElementById('form').addEventListener('submit', handleSubmit);
+    document.getElementById('reload').addEventListener('click', playingNewGame);
+    document.getElementById('end-of-game').addEventListener('click', endOfTheGame); 
 
     function openingWordVersion() {
         wordVers.addEventListener('click', function() {
             const panel = this.nextElementSibling;
             
-            if (panel.style.display === 'block') {
-                panel.style.display = 'none';
-            } else {
-                panel.style.display = 'block';
-            }
+            (panel.style.display === 'block') ? panel.style.display = 'none' : panel.style.display = 'block';
         });
     }
     openingWordVersion();
@@ -31,22 +33,15 @@
         return arr[randomIsogramWord];
     }
 
-    const arrayOfIsograms = ['pie', 'bar', 'end', 'duty', 'dust', 'host', 'blast', 'store', 'layer', 'market', 'sailor', 'mosaic'];
-    const x = getRandomArrayValue(arrayOfIsograms);
-
-    console.log('Isogram (secret word) is ' + x);
-
-    function countTheNumbersOfTries() {
-        return Math.round(x.length*1.5);
+    function countTheNumbersOfTries(isogram) {
+        return Math.round(isogram.length*1.5);
     }
 
-    let tries = countTheNumbersOfTries();
- 
     function calculateBullsAndCows(isogram, attempt) {
         let guessBulls = 0;
         let guessCows = 0;
          
-            for (let i = 0; i < x.length; i++) {
+            for (let i = 0; i < isogram.length; i++) {
                 if (isogram[i] === attempt[i]) {
                     guessBulls++;
                 } else if (isogram.indexOf(attempt[i])>=0) {
@@ -57,31 +52,31 @@
         return { guessBulls, guessCows };
     }
 
-    const { guessBulls, guessCows } = calculateBullsAndCows(x, formInput.value);
+    const { guessBulls, guessCows } = calculateBullsAndCows(isogramWord, formInput.value);
 
     function insertElements(newNode, referenceNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
     }
 
-    function creationOfElements() {
+    function creationOfElements(isogram, tries, bulls, cows) {
         const length = document.createElement('p');
-        length.innerHTML = `Isogram (secret word) consists of <strong class='red'> ${x.length} </strong> letters`;
-        insertElements(length, letsPlay);
+        length.innerHTML = `Isogram (secret word) consists of <strong class='red'> ${isogram.length} </strong> letters`;
+        insertElements(length, document.getElementById('letsPlay'));
 
-        const info = document.createElement('p');
-        info.innerHTML = `<strong id='input-info' class='red list'></strong>`;
-        insertElements(info, btnSubmit);   
+        const info = document.createElement('div');
+        info.innerHTML = `<ul id='input-info' class='red list'></ul >`;
+        insertElements(info, document.getElementById('form'));   
 
         const attempts = document.createElement('p');
         attempts.innerHTML = `You have <strong id='tries' class='red'> ${tries} </strong> attempts left from <strong class='red'> ${tries} </strong>`;
         insertElements(attempts, document.getElementById('input-info'));
 
         const guessing = document.createElement('p');
-        guessing.innerHTML = `You guessed <strong id='bulls' class='red'> ${guessBulls} </strong> bulls and <strong id='cows' class='red'> ${guessCows} </strong> cows!`;
+        guessing.innerHTML = `You guessed <strong id='bulls' class='red'> ${bulls} </strong> bulls and <strong id='cows' class='red'> ${cows} </strong> cows!`;
         insertElements(guessing, attempts);
 
-        const historyTries = document.createElement('p');
-        historyTries.innerHTML = `<strong id='history-tries' class='list'></strong>`;
+        const historyTries = document.createElement('div');
+        historyTries.innerHTML = `<ul id='history-tries' class='list'></ul>`;
         insertElements(historyTries, validTries);
 
         const win = document.createElement('h2');
@@ -96,7 +91,7 @@
         thanks.innerHTML = `<div id='thanks' class='red'></div>`;
         insertElements(thanks, document.getElementById('end-of-game')); 
     }
-    creationOfElements();
+    creationOfElements(isogramWord, tries, guessBulls, guessCows);
 
     let triesElem = document.getElementById('tries');
     const bulls = document.getElementById('bulls');
@@ -118,11 +113,11 @@
 
     function checkUserInput(value) {
         const errors = [];
-        let regex = /[a-z]/g;
+        const regex = /[a-z]/g;
         let found = value.match(regex);
        
-        if (value.length !== x.length) {
-            errors.push('Enter the word consisting of ' + x.length + ' letters' + '!');
+        if (value.length !== isogramWord.length) {
+            errors.push('Enter the word consisting of ' + isogramWord.length + ' letters' + '!');
         }
         if (!charCheck(value)) {
             errors.push('Enter the isogram word!');
@@ -134,22 +129,16 @@
     }
     
     function decreaseOfAttempts() {
-        if (tries > 1) {
-            return --tries;
-        }
-        return 0;
+        return (tries > 0) ? --tries : 0
     }
 
     function renderArrayAndElement(elem, arr) {
         let newHTML = '';
         for (let i = 0; i < arr.length; i++) {
-            const c = arr[i];
-            newHTML += `<li>${c}</li>`;
+            newHTML += `<li><strong>${arr[i]}</strong></li>`;
         }
         elem.innerHTML = newHTML;
     }
-
-    const validResults = [];
 
     function handleSubmit() {
         const inputValue = formInput.value;
@@ -162,15 +151,15 @@
 
         let decrease = decreaseOfAttempts();
         triesElem.innerHTML = decrease;
-        const calculationOfBAndC = calculateBullsAndCows(x, inputValue);
+        const calculationOfBAndC = calculateBullsAndCows(isogramWord, inputValue);
         const result = `${inputValue} ${calculationOfBAndC.guessBulls}B${calculationOfBAndC.guessCows}C`;
         inputInfo.innerHTML = '';
         bulls.innerHTML = calculationOfBAndC.guessBulls;
         cows.innerHTML = calculationOfBAndC.guessCows;
         validResults.push(result);
         renderArrayAndElement(historyTries, validResults);
-        showWinOrLose(x, inputValue, decrease);
-        displayPlayAgain(x, inputValue, decrease);
+        showWinOrLose(isogramWord, inputValue, decrease);
+        displayPlayAgain(isogramWord, inputValue, decrease);
     }
     
     function showWinOrLose(isogram, attempt, decrease) {
